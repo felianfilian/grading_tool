8602d00973bf2318a4c6eeba06591ac8adf16331
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-01 18:55:24 +0100

    grade project solved

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 student_scores = {
     "Peter" : 83,
-    "Luigi" : 71
+    "Luigi" : 76,
+    "Mario" : 92,
+    "Bowser" : 52
 }
+
+for student in student_scores:
+    if student_scores[student] >= 91:
+        print(student + " - excelent")
+    elif student_scores[student] >= 81:
+        print(student + " - good")
+    elif student_scores[student] >= 71:
+        print(student + " - ok")
+    else:
+        print(student + " - fail")
